a63d7efa7bd9c75f5b8b8a300e269944d484a166
Harikrishnan <harikrishnan@mallow-tech.com>
2023-09-03 09:02:17 +0530

    Initial file setup and added hilt support

--- app/src/main/java/com/example/multicastndsapplication/MainActivity.kt
+++ app/src/main/java/com/example/multicastndsapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.multicastndsapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.multicastndsapplication.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainActivityBinding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainActivityBinding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(this.root)
+        }
     }
 }
